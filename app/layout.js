358470d0c180e62b1bd9a1d5a067db6fbbe758be
Yezir971 <james_ahmedaly@yahoo.com>
2025-06-22 02:11:46 +0200

    feat : init compatibility pwa

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import { Poppins } from 'next/font/google'
 import './globals.css'
 import { AuthProvider } from '@/context/authContext'
-import NavBar from '@/components/NavBar'
 import ArticleProvider from '@/context/articleContext'
+import NavBar from '@/components/navBar'
 
 const popins = Poppins({
     variable: '--font-popins',
@@ -10,9 +10,35 @@ const popins = Poppins({
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
 })
 
+export const metadata = {
+    title: 'Hors zone',
+    description: 'Application web progressive',
+}
+
 export default function RootLayout({ children }) {
     return (
         <html lang="fr">
+            <head>
+                <meta charSet="utf-8" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <meta name="theme-color" content="#0080FF" />
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="icon" href="/icons/icon-192x192.png" />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="192x192"
+                    href="/images/logo/web-app-manifest-192x192.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="512x512"
+                    href="/icons/icon-512x512.png"
+                />
+                <meta name="apple-mobile-web-app-title" content="hors zone" />
+            </head>
             <body className={`${popins.variable} antialiased`}>
                 <AuthProvider>
                     <ArticleProvider>
