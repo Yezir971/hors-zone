5009e49f2a55ab7617a856f61fa0728c2dc552c5
Yezir971 <james_ahmedaly@yahoo.com>
2025-06-25 02:55:10 +0200

    fix : fix bug map and light night mode

--- components/detail/Map.js
+++ components/detail/Map.js
@@ -1,7 +1,10 @@
 'use client'
 
+import { useTheme } from '@/context/themeContext'
 import { GoogleMap, MarkerF, useJsApiLoader } from '@react-google-maps/api'
 import { useCallback, useState } from 'react'
+import Loading from '../Loading'
+
 const containerStyle = {
     width: '100%',
     height: '100%',
@@ -10,6 +13,7 @@ const containerStyle = {
 }
 
 const Map = ({ long, lat, titre }) => {
+    const { theme } = useTheme()
     const center = {
         lat: lat,
         lng: long,
@@ -27,10 +31,94 @@ const Map = ({ long, lat, titre }) => {
         setMap(null)
     }, [])
 
+    const nightStyle = [
+        { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+        { elementType: 'labels.text.stroke', stylers: [{ color: '#242f3e' }] },
+        { elementType: 'labels.text.fill', stylers: [{ color: '#746855' }] },
+        {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }],
+        },
+        {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }],
+        },
+        {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{ color: '#263c3f' }],
+        },
+        {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#6b9a76' }],
+        },
+        {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{ color: '#38414e' }],
+        },
+        {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{ color: '#212a37' }],
+        },
+        {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#9ca5b3' }],
+        },
+        {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{ color: '#746855' }],
+        },
+        {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{ color: '#1f2835' }],
+        },
+        {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#f3d19c' }],
+        },
+        {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{ color: '#2f3948' }],
+        },
+        {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#d59563' }],
+        },
+        {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{ color: '#17263c' }],
+        },
+        {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{ color: '#515c6d' }],
+        },
+        {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{ color: '#17263c' }],
+        },
+    ]
+
     return isLoaded ? (
         <GoogleMap
-            mapContainerStyle={containerStyle}
             center={center}
+            mapContainerStyle={containerStyle}
+            options={{
+                styles: theme === 'dark' ? nightStyle : [],
+            }}
             zoom={10}
             onLoad={onLoad}
             onUnmount={onUnmount}
@@ -45,7 +133,12 @@ const Map = ({ long, lat, titre }) => {
             />
         </GoogleMap>
     ) : (
-        <></>
+        <div
+            style={containerStyle}
+            className="flex items-center justify-center bg-gray-100 dark:bg-gray-800"
+        >
+            <Loading />
+        </div>
     )
 }
 
